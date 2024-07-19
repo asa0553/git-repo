7720bbc47a7cff78c1266e1219b26b8d5f73fafa
Obed Asamoah Agyei <aobed117@gmail.com>
2024-07-19 10:51:02 +0000

    message

--- Client.java
+++ Client.java
@@ -2,4 +2,7 @@ public class Client {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
+    static void sendMessage(String message) {
+        System.out.println("Sending message: " + message);
+    }
 }
